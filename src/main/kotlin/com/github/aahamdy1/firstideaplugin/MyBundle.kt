989728c873a9e5ed2bf5250ecda12137859b23bd
GitHub Action <action@github.com>
2026-04-18 16:21:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/aahamdy1/firstideaplugin/MyBundle.kt
+++ src/main/kotlin/com/github/aahamdy1/firstideaplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.aahamdy1.firstideaplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/aahamdy1/firstideaplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/aahamdy1/firstideaplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.aahamdy1.firstideaplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
